fbd1ac6b5e3e5dd3e289f52a7d9181e89a7c8c18
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-07-28 12:09:08 -0300

    TEST: created get tests on the backend

--- back-end/tests/factory/recommendationFactory.ts
+++ back-end/tests/factory/recommendationFactory.ts
@@ -3,8 +3,9 @@ import { faker } from "@faker-js/faker"
 
 import { prisma } from "../../src/database.js"
 import app from "../../src/app.js"
+import { recommendationRepository } from "../../src/repositories/recommendationRepository.js"
 
-export async function createRecommendation() {
+export async function createRecommendationAndReturnLink() {
   const data = {
     name: faker.random.words(3),
     youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(5)}`,
@@ -14,4 +15,17 @@ export async function createRecommendation() {
   const link = await prisma.recommendation.findFirst({ where: { name: data.name } })
 
   return { data, link }
+}
+
+export async function createRecommendation() {
+  const data = {
+    name: faker.random.words(3),
+    youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(5)}`,
+  }
+
+  await supertest(app).post("/recommendations").send(data)
+}
+
+export async function upvoteRecommendation(id: number) {
+  await recommendationRepository.updateScore(id, "increment")
 }
---
+++ back-end/tests/integration/app.test.ts
@@ -0,0 +1,171 @@
+import supertest from "supertest"
+import { faker } from "@faker-js/faker"
+
+import app from "../../src/app.js"
+import { prisma } from "../../src/database.js"
+import * as Factory from "../factory/recommendationFactory.js"
+
+beforeEach(async () => {
+  await prisma.recommendation.deleteMany()
+})
+
+afterEach(async () => {
+  await prisma.$disconnect()
+})
+
+afterAll(async () => {
+  await prisma.recommendation.deleteMany()
+})
+
+describe("POST /recommendations", () => {
+  it("return 201 with post created", async () => {
+    const data = {
+      name: faker.random.words(3),
+      youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(5)}`,
+    }
+
+    const response = await supertest(app).post("/recommendations").send(data)
+    const link = await prisma.recommendation.findFirst({ where: { name: data.name } })
+
+    expect(response.status).toBe(201)
+    expect(link).not.toBeNull()
+  })
+
+  it("return 422 with wrong url", async () => {
+    const data = {
+      name: faker.random.words(3),
+      youtubeLink: `https://www.google.com`,
+    }
+
+    const response = await supertest(app).post("/recommendations").send(data)
+    const link = await prisma.recommendation.findFirst({ where: { name: data.name } })
+
+    expect(response.status).toBe(422)
+    expect(link).toBeNull()
+  })
+
+  it("return 422 without data", async () => {
+    const data = {}
+
+    const response = await supertest(app).post("/recommendations").send(data)
+
+    expect(response.status).toBe(422)
+  })
+})
+
+describe("POST /recommendations/:id/upvote", () => {
+  it("return 200 with upvote", async () => {
+    const { data, link } = await Factory.createRecommendationAndReturnLink()
+
+    const response = await supertest(app).post(`/recommendations/${link.id}/upvote`)
+    const newLink = await prisma.recommendation.findUnique({ where: { id: link.id } })
+
+    expect(response.status).toBe(200)
+    expect(newLink.score).toStrictEqual(1)
+  })
+})
+
+describe("POST /recommendations/:id/downvote", () => {
+  it("return 200 with five downvotes", async () => {
+    const { data, link } = await Factory.createRecommendationAndReturnLink()
+
+    for (let i = 0; i < 4; i++) {
+      await supertest(app).post(`/recommendations/${link.id}/downvote`)
+    }
+
+    const response = await supertest(app).post(`/recommendations/${link.id}/downvote`)
+    const newLink = await prisma.recommendation.findUnique({ where: { id: link.id } })
+
+    expect(response.status).toBe(200)
+    expect(newLink.score).toStrictEqual(-5)
+  })
+
+  it("return 200 and delete recommendation with six downvotes", async () => {
+    const { data, link } = await Factory.createRecommendationAndReturnLink()
+
+    for (let i = 0; i < 5; i++) {
+      await supertest(app).post(`/recommendations/${link.id}/downvote`)
+    }
+
+    const response = await supertest(app).post(`/recommendations/${link.id}/downvote`)
+    const newLink = await prisma.recommendation.findUnique({ where: { id: link.id } })
+
+    expect(response.status).toBe(200)
+    expect(newLink).toBeNull()
+  })
+})
+
+describe("GET /recommendations", () => {
+  it("return 200 with array of 10 recommendations", async () => {
+    for (let i = 0; i <= 10; i++) {
+      await Factory.createRecommendation()
+    }
+
+    const response = await supertest(app).get("/recommendations")
+
+    expect(response.status).toBe(200)
+    expect(response.body.length).toBe(10)
+  })
+})
+
+describe("GET /recommendations/:id", () => {
+  it("return 200 with specified recommendation", async () => {
+    const { link } = await Factory.createRecommendationAndReturnLink()
+
+    const response = await supertest(app).get(`/recommendations/${link.id}`)
+
+    expect(response.status).toBe(200)
+    expect(response.body.id).toStrictEqual(link.id)
+  })
+
+  it("return 404 with wrong id", async () => {
+    const { link } = await Factory.createRecommendationAndReturnLink()
+
+    const response = await supertest(app).get(`/recommendations/${link.id + 1}`)
+
+    expect(response.status).toBe(404)
+    expect(response.body.id).toBeUndefined()
+  })
+})
+
+// TODO: finish this route
+describe("GET /recommendations/random", () => {
+  it("return 404 without any recommendation", async () => {
+    const response = await supertest(app).get("/recommendations/random")
+    expect(response.status).toBe(404)
+    expect(response.body.id).toBeUndefined()
+  })
+})
+
+describe("GET /recommendations/top/:amount", () => {
+  it("return array of 5 racommendations by score", async () => {
+    for (let i = 0; i < 5; i++) {
+      const { link } = await Factory.createRecommendationAndReturnLink()
+      for (let j = i; j < 5; j++) {
+        await Factory.upvoteRecommendation(link.id)
+      }
+    }
+
+    const response = await supertest(app).get("/recommendations/top/5")
+
+    expect(response.status).toBe(200)
+    expect(response.body.length).toStrictEqual(5)
+    expect(response.body[0].score).toStrictEqual(5)
+    expect(response.body[1].score).toStrictEqual(4)
+  })
+
+  it("return array of 2 racommendations by score", async () => {
+    for (let i = 0; i < 5; i++) {
+      const { link } = await Factory.createRecommendationAndReturnLink()
+      for (let j = i; j < 5; j++) {
+        await Factory.upvoteRecommendation(link.id)
+      }
+    }
+
+    const response = await supertest(app).get("/recommendations/top/2")
+
+    expect(response.status).toBe(200)
+    expect(response.body.length).toStrictEqual(2)
+    expect(response.body[1].score).toStrictEqual(4)
+  })
+})
